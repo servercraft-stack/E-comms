d417c0e97cbc67221231edb593b3a576809af5d7
David Onyekachi <davidonyekachi@customer.lgosnga1.isp.starlink.com>
2026-03-27 13:53:48 +0100

    updated build.sh3

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -236,3 +238,6 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+
